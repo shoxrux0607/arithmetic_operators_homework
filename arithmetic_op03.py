e2d5374e4eef283a83b41c2bb674de84c8068749
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-26 15:29:37 +0500

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 26
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer  = number % 10 *10 + number // 10
 #Print the value of the 'answer'
+print(answer)
